8e6f4cfe5798263cca1543fb350b75c2fe6b8e28
Pouria Ebram <46888201+Pouria-Ebram@users.noreply.github.com>
2019-03-06 20:21:49 +1100

    Update task3_30035678.py

--- task3_30035678.py
+++ task3_30035678.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-FIT9133 Programming foundations in python assignment 2
-
 Task 3: Building a Class for Data Visualisation
 
 Created on Fri Sep 28 16:48:59 2018
 
 Last edited on Oct 05 14:00 2018
 
-@author: Pouria Ebrahimnezhad
-
-STUID: 30035678
+@author: Pouria Ebram
 
 """
 import numpy as np
